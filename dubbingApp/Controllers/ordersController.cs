4acc0fe5c6be5c55cfec932945ba5c0cd3bb9697
Wael <wael.alshatti@gmail.com>
2016-05-20 20:03:47 +0300

    Order planning & Adaptation Assignment Added to Order Management

--- dubbingApp/Controllers/ordersController.cs
+++ dubbingApp/Controllers/ordersController.cs
@@ -196,7 +196,70 @@ namespace dubbingApp.Controllers
         // planning dubbing, upload and shipment
         public ActionResult orderItemSelectList(long id)
         {
+            long work = db.orderTrnHdrs.Find(id).workIntno;
+            ViewBag.personnelList = new SelectList(db.workPersonnels.Include(b => b.employee)
+                                .Where(b => b.workIntno == work && (b.titleType == "04" || b.titleType == "06"))
+                                .Select(b => new { b.employee.empIntno, b.employee.fullName }), "empIntno", "fullName");
             return PartialView("_orderItemSelectList");
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost, ValidateInput(false)]
+        public ActionResult scheduleAddNew(string oiListHF, DateTime? scheduleDate, string scheduleTypeHF, long? personnel)
+        {
+            if (scheduleDate.HasValue)
+            {
+                var model = db.orderTrnHdrs;
+                string[] oiList = oiListHF.Split(';');
+
+                // input data validation
+                var z = LookupModels.decodeDictionaryItem("settings", "fdw").ToUpper();
+                DateTime dt = scheduleDate.Value;
+                if (scheduleTypeHF == "01" && dt.DayOfWeek.ToString().ToUpper() != z)
+                    return Content("The Date for Scheduling MUST Start on " + z + ". ", "text/html");
+                if (scheduleTypeHF == "04")
+                {
+                    if (!personnel.HasValue)
+                        return Content("Please Provide the Adapation Personnel. ", "text/html");
+                    long work = db.orderTrnHdrs.Find(long.Parse(oiList[0])).workIntno;
+                    for (int i = 1; i < oiList.Count(); i++)
+                    {
+                        long tmp = db.orderTrnHdrs.Find(long.Parse(oiList[i])).workIntno;
+                        if (tmp != work)
+                            return Content("Failed! Selected Episodes Belong to Different Works. Please Correct Selection. ", "text/html");
+                    }
+                }
+                
+                foreach(string item in oiList)
+                {
+                    long id = long.Parse(item);
+                    switch (scheduleTypeHF)
+                    {
+                        case "01":
+                            model.Find(id).plannedDubbing = scheduleDate.Value;
+                            break;
+                        case "02":
+                            model.Find(id).plannedUpload = scheduleDate.Value;
+                            break;
+                        case "03":
+                            model.Find(id).plannedShipment = scheduleDate.Value;
+                            break;
+                        case "04":
+                            model.Find(id).startAdaptation = scheduleDate.Value;
+                            var model1 = db.orderTrnDtls;
+                            orderTrnDtl dtl = new orderTrnDtl();
+                            dtl.orderTrnHdrIntno = id;
+                            dtl.activityType = "02";
+                            dtl.empIntno = personnel.Value;
+                            model1.Add(dtl);
+                            break;
+                    }
+                }
+                db.SaveChanges();
+            }
+            else
+                return Content("Please Select a Date for Scheduling. ", "text/html");
+            return Content("Successfully Scheduled. ", "text/html");
         }
     }
 }
