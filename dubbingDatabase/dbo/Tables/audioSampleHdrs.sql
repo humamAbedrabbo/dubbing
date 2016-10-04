fb8983f2d938fadf910afc2598974fd78d4f2496
Humam <humam.abedrabbo@hotmail.com>
2016-10-04 23:04:46 +0300

    bug fix in voiceActors name in sql scripts

--- dubbingDatabase/dbo/Tables/audioSampleHdrs.sql
+++ dubbingDatabase/dbo/Tables/audioSampleHdrs.sql
@@ -2,7 +2,7 @@
 (
 	[audioSampleHdrIntno] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
     [Title] NVARCHAR(250) NOT NULL, 
-	[VoiceActorIntno] bigint NOT NULL,
+	[voiceActorIntno] bigint NOT NULL,
 	[tagTemplateHdrIntno] bigint NOT NULL,
     [SubmitDate] DATETIME NOT NULL DEFAULT getdate(), 
     [FileName] NVARCHAR(250) NULL, 
@@ -11,5 +11,5 @@
     [Description] NVARCHAR(MAX) NULL, 
     [Status] NVARCHAR(10) NULL, 
     CONSTRAINT [FK_audioSampleHdrs_To_tagTemplateHdrs] FOREIGN KEY ([tagTemplateHdrIntno]) REFERENCES [tagTemplateHdrs]([tagTemplateHdrIntno]), 
-    CONSTRAINT [FK_audioSampleHdrs_To_voiceActors] FOREIGN KEY ([VoiceActorIntno]) REFERENCES [voiceActors]([VoiceActorIntno]) on delete cascade
+    CONSTRAINT [FK_audioSampleHdrs_To_voiceActors] FOREIGN KEY ([voiceActorIntno]) REFERENCES [voiceActors]([voiceActorIntno]) on delete cascade
 )
