d486838052a9e95eda2d08f88f920c8d31ce12dc
Humam <humam.abedrabbo@hotmail.com>
2016-10-12 22:36:40 +0300

    vl-tags complete feature

--- voiceLibrary/Controllers/tagsController.cs
+++ voiceLibrary/Controllers/tagsController.cs
@@ -15,8 +15,40 @@ namespace voiceLibrary.Controllers
         // GET: tags
         public ActionResult Index()
         {
-            var model = ctx.tags.Include(x => x.tagType);
+            
+            ViewBag.tagTypes = ctx.tagTypes.OrderBy(x=>x.Name);
             return View();
+        }
+
+        public ActionResult tagList(int typeId = 0)
+        {
+            var model = ctx.tags.Where(x=>x.TagTypeId == typeId).OrderBy(x=>x.Name);
+
+            return PartialView("_tagList", model);
+        }
+
+        public void saveTag(int tagId, string name)
+        {
+            var tag = ctx.tags.Find(tagId);
+            tag.Name = name;
+            ctx.SaveChanges();
+        }
+
+        public ActionResult deleteTag(int tagId, int typeId)
+        {
+            var tag = ctx.tags.Find(tagId);
+            ctx.tags.Remove(tag);
+            ctx.SaveChanges();
+            return tagList(typeId);
+        }
+        public ActionResult saveNewTag(string name, int typeId)
+        {
+            var tag = ctx.tags.Create();
+            tag.Name = name;
+            tag.TagTypeId = typeId;
+            ctx.tags.Add(tag);
+            ctx.SaveChanges();
+            return tagList(typeId);
         }
     }
 }
---
+++ voiceLibrary/Scripts/VL/vl.js
@@ -0,0 +1,77 @@
+﻿
+// Template functions
+
+// Tag functions
+function dispTags() {
+    var typeId = $("#tag_type_selector > option")[tag_type_selector.selectedIndex].value;
+    var url = '/tags/tagList';
+    $.ajax({
+        contentType: 'text/html',
+        method: 'GET',
+        url: url,
+        data: { typeId: typeId },
+        success: function (result) {
+            $("#tag_list").empty();
+            $("#tag_list").html(result);
+        }
+    });
+}
+function editTag(tagId) {
+    $(".tagLabel[data-id='" + tagId + "']").toggleClass('hidden');
+    $(".tag[data-id='" + tagId + "']").toggleClass('hidden');
+    $("#saveTag[data-id='" + tagId + "']").toggleClass('hidden');
+}
+function saveTag(tagId) {
+    var tagName = $(".tag[data-id='" + tagId + "']").val();
+    var url = '/tags/saveTag';
+    $.ajax({
+        contentType: 'text/html',
+        method: 'GET',
+        url: url,
+        data: { tagId: tagId, name: tagName },
+        success: function (result) {
+            $("#saveTag[data-id='" + tagId + "']").toggleClass('hidden');
+            $(".tagLabel[data-id='" + tagId + "']").text(tagName);
+            $(".tagLabel[data-id='" + tagId + "']").toggleClass('hidden');
+            $(".tag[data-id='" + tagId + "']").toggleClass('hidden');
+        }
+    });
+}
+function deleteTag(tagId) {
+    var tagName = $(".tag[data-id='" + tagId + "']").val();
+    if (!confirm('Are you sure you want to delete [' + tagName + "]?"))
+        return
+
+    var typeId = $("#tag_type_selector > option")[tag_type_selector.selectedIndex].value;
+    var url = '/tags/deleteTag';
+    $.ajax({
+        contentType: 'text/html',
+        method: 'GET',
+        url: url,
+        data: { tagId: tagId, typeId: typeId },
+        success: function (result) {
+            $("#tag_list").empty();
+            $("#tag_list").html(result);
+        }
+    });
+}
+function saveNewTag() {
+    var tagName = $("#newTagName").val();
+    var typeId = $("#tag_type_selector > option")[tag_type_selector.selectedIndex].value;
+    if (typeId == 0)
+    {
+        alert("You must select a tag type");
+        return;
+    }
+    var url = '/tags/saveNewTag';
+    $.ajax({
+        contentType: 'text/html',
+        method: 'GET',
+        url: url,
+        data: { name: tagName, typeId: typeId },
+        success: function (result) {
+            $("#tag_list").empty();
+            $("#tag_list").html(result);
+        }
+    });
+}
