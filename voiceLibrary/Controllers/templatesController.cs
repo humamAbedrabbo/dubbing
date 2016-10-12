9edf8be5f40f37fdb8f46beb820ae9c8920b2c34
Humam <humam.abedrabbo@hotmail.com>
2016-10-12 23:42:41 +0300

    vl-templates re-evaluated

---
+++ voiceLibrary/Controllers/templatesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dubbingModel;
+using System.Data.Entity;
+
+namespace voiceLibrary.Controllers
+{
+    public class templatesController : Controller
+    {
+        DUBBDBEntities ctx = new DUBBDBEntities();
+
+        // GET: templates
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult templateList()
+        {
+            var model = ctx.tagTemplateHdrs.Include(x=>x.tagTemplateDtls);
+            return PartialView("_templateList", model);
+        }
+    }
+}
