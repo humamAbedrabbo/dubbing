64e5d3c0b773f9dab8b8b08cdfe0f45d225a2c60
Humam <humam.abedrabbo@hotmail.com>
2016-10-12 19:48:32 +0300

    added initial tags for VL

---
+++ voiceLibrary/Controllers/tagsController.cs
@@ -0,0 +1,22 @@
+﻿using dubbingModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace voiceLibrary.Controllers
+{
+    public class tagsController : Controller
+    {
+        DUBBDBEntities ctx = new DUBBDBEntities();
+
+        // GET: tags
+        public ActionResult Index()
+        {
+            var model = ctx.tags.Include(x => x.tagType);
+            return View();
+        }
+    }
+}
