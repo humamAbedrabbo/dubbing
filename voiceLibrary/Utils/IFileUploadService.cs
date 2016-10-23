b8b7bb0275d05824286a5c359e68fd501ea3b048
Humam <humam.abedrabbo@hotmail.com>
2016-10-24 01:04:31 +0300

    initial audio samples with local file upload strategy added

---
+++ voiceLibrary/Utils/IFileUploadService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace voiceLibrary.Utils
+{
+    public interface IFileUploadService
+    {
+        string UploadFile(HttpPostedFileBase fileInput, string fileName);
+    }
+}
